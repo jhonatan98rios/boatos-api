cf8e2f1d41c6948284fc5a8c57e8a680dd393512
Jhonatan Teixeira Rios <jhonatan.rios@the9level.com>
2021-02-22 08:50:42 -0300

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const fakeApi = require('./fakeApi')
+
+app.get('/', (req, res) => {
+
+  const selectedNews = []
+  const indexes = []
+
+  for(let i=0; i < 3; i++){
+    let randomValue = ~~(Math.random() * 11)
+
+    if(indexes.indexOf(randomValue) == -1){
+      indexes.push(randomValue)
+    }else {
+      if(randomValue < 9){
+        indexes.push(randomValue + 1)
+      }else {
+        indexes.push(randomValue - 1)
+      }
+    }
+  }
+
+  indexes.map(index => {
+    selectedNews.push(fakeApi[index])
+  })
+
+  res.send(selectedNews)
+})
+
+app.listen(3000)
